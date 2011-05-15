3da15f184308be77812e3f91caf8e69b780090bf
Grant McLean <grant@mclean.net.nz>
2011-05-15 22:09:10 +1200

    start index page and layout from scratch with JS lazy loading support

--- lib/TKS/Web.pm
+++ lib/TKS/Web.pm
@@ -1,11 +1,19 @@
 package TKS::Web;
 
 use Dancer ':syntax';
+use TKS::Web::Helpers;
 
 our $VERSION = '0.1';
+
+
+get '/' => sub {
+    js_file "js/lazyload-min.js";
+    template 'index', {};
+};
+
 
 get '/dancer' => sub {
     template 'dancer', {}, { layout => 'dancer' };
 };
 
-true;
+1;
---
+++ lib/TKS/Web/Helpers.pm
@@ -0,0 +1,26 @@
+package TKS::Web::Helpers;
+
+use Dancer ':syntax';
+
+use Exporter qw(import);
+
+our @EXPORT = qw( &js_file );
+
+
+before sub {
+    var js_files => [];
+};
+
+
+before_template sub {
+    my $tokens = shift;
+    $tokens->{js_files} = var 'js_files';
+};
+
+
+sub js_file {
+    my $js = var 'js_files';
+    push @$js, @_;
+}
+
+1;
