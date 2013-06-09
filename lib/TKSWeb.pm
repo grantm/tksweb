eb8d5458d58a3982666e5baa70ceeb8034b54557
Grant McLean <grant@mclean.net.nz>
2013-06-09 19:38:30 +1200

    implement a simple login

--- lib/TKSWeb.pm
+++ lib/TKSWeb.pm
@@ -2,6 +2,7 @@ package TKSWeb;
 
 use Dancer ':syntax';
 use Dancer::Plugin::DBIC;
+use Dancer::Plugin::Passphrase;
 
 use TKSWeb::Schema;
 
@@ -11,14 +12,79 @@ our $VERSION = '0.1';
 sub User { schema->resultset('AppUser'); }
 
 
-get '/' => sub {
-    my $user = user_by_email('grant@mclean.net.nz');
-    template 'week-view', { user => $user->full_name };
+##################################  Hooks  ###################################
+
+hook before => sub {
+    if( my $user = user_by_email( session('email') ) ) {
+        var user => $user;
+        return;
+    }
+    if( request->path !~ m{^/(login|logout)$} ) {
+        redirect "/login";
+    }
 };
 
 
+hook before_template_render => sub {
+    my $tokens = shift;
+
+    my $vars = vars;
+    $tokens->{user}  = $vars->{user};
+    $tokens->{alert} = $vars->{alert};
+};
+
+
+################################  Helpers  ###################################
+
+sub alert {
+    my($message) = @_;
+    var alert => $message;
+}
+
+
+################################  Routes  ####################################
+
+get '/login' => sub {
+    template 'login';
+};
+
+
+post '/login' => sub {
+    my $user = get_user_from_login( param('email'), param('password') );
+    if( $user ) {
+        session email => $user->email;
+        redirect '/';
+    }
+    alert 'Invalid username or password';
+    template 'login', { email => param('email') };
+};
+
+
+get '/logout' => sub {
+    session->destroy;
+    redirect "/login";
+};
+
+
+get '/' => sub {
+    template 'week-view';
+};
+
+
+sub get_user_from_login {
+    my($email, $password) = @_;
+
+    return unless $email;
+    my $user = user_by_email( $email );
+    if( $user  and  passphrase($password)->matches($user->password) ) {
+        return $user;
+    }
+    return;
+}
+
+
 sub user_by_email {
-    my($email) = @_;
+    my $email = shift or return;
     return User->search({
         email   => lc( $email ),
         status  => 'active',
