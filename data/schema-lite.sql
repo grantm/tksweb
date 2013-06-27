6f7f6b60b9dbb807eccba461d4727a90ad42b6fb
Grant McLean <grant@mclean.net.nz>
2013-06-28 06:43:09 +1200

    don't allow nulls in wr_system.colour_code

--- data/schema-lite.sql
+++ data/schema-lite.sql
@@ -16,7 +16,7 @@ CREATE TABLE wr_system (
     app_user_id     INTEGER NOT NULL,
     name            TEXT    NOT NULL,
     description     TEXT    NOT NULL,
-    colour_code     INTEGER,
+    colour_code     INTEGER NOT NULL DEFAULT '0',
 
     CONSTRAINT FK_WR_SYSTEM_USER
         FOREIGN KEY (app_user_id) REFERENCES app_user (app_user_id)
