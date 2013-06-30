b2cf7365c2de10ca6cb002ee5266e668e7e0db35
Grant McLean <grant@mclean.net.nz>
2013-06-30 15:24:24 +1200

    add an API key to user in sample data

--- data/sample-data.sql
+++ data/sample-data.sql
@@ -3,7 +3,16 @@ DELETE FROM app_user;
 DELETE FROM wr_system;
 DELETE FROM activity;
 
-INSERT INTO app_user VALUES(1,'grant@mclean.net.nz','Grant McLean','{CRYPT}$2a$04$qUPNseqxQZFLWaNu4C5a8eA8kiK3Rp/CxonoBpK.lcF3g0Tu83KHG',NULL,NULL,'active',1);
+INSERT INTO app_user VALUES(
+    1,
+    'grant@mclean.net.nz',
+    'Grant McLean',
+    '{CRYPT}$2a$04$qUPNseqxQZFLWaNu4C5a8eA8kiK3Rp/CxonoBpK.lcF3g0Tu83KHG',
+    NULL,
+    '8dabbc494e28f2f8577a425963d11c454065ed57',
+    'active',
+    1
+);
 
 INSERT INTO wr_system VALUES (1,1,'catalyst','Catalyst WRMS',1);
 INSERT INTO wr_system VALUES (2,1,'eec','EEC WRMS',1);
