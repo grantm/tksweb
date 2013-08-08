e19dd3edf8f8c11c16556fd407d5f99bd0079b79
Grant McLean <grant@mclean.net.nz>
2013-08-08 17:20:03 +1200

    update comments and sample user column values

--- data/add_user.sql
+++ data/add_user.sql
@@ -1,20 +1,23 @@
--- generate a value for API-KEY with a command like this:
--- ps fax | sort | sha1sum -
+-- Change these fields:
+--   USER      - email address (2 places)
+--   FULL_NAME - user's full name
+--   API_KEY   - generate a unique value with a command like this:
+--               ps fax | sort | sha1sum -
 --
--- Change email address (2 places) and full name
+-- Use the password reset function on the login page.
 
 INSERT INTO app_user (
     email, full_name, password, reset_key, api_key, status, admin
 ) VALUES (
-    'user@catalyst.net.nz', 'Full Name', '*LOCKED*', NULL,
-    'API-KEY',
+    'USER@catalyst.net.nz', 'FULL_NAME', '*LOCKED*', NULL,
+    'API_KEY',
     'active', 0
 );
 
 INSERT INTO wr_system (
     app_user_id, name, description, request_url
 ) VALUES (
-    (SELECT app_user_id FROM app_user WHERE email = 'user@mclean.net.nz'),
+    (SELECT app_user_id FROM app_user WHERE email = 'USER@catalyst.net.nz'),
     'catalyst', 'Catalyst WRMS',
     'https://wrms.catalyst.net.nz/wr.php?request_id='
 );
