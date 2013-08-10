28e7c773b51e7e022e43f295445d17290b995e5a
Grant McLean <grant@mclean.net.nz>
2013-08-10 15:19:28 +1200

    fix up the example export link in the help page

--- lib/TKSWeb.pm
+++ lib/TKSWeb.pm
@@ -134,8 +134,8 @@ post '/login' => sub {
 
 get '/help' => sub {
     my $user = var 'user';
-    my $date = _now()->strftime('%Y-%m-01');
-    my $sample_url = base_url . "/reset/export/catalyst/$date.tks";
+    my $date = _now()->strftime('%Y-%m');
+    my $sample_url = base_url . "/export/catalyst/$date.tks";
     template 'help', { api_key => $user->api_key, export_url => $sample_url };
 };
 
