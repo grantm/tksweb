f5550be481017576f3d46bcb6a789803960188e0
Sam Crawley <samc@catalyst.net.nz>
2013-11-26 15:28:47 +1300

    Added postgres schema, and added missing table to sqlite schema.

--- data/schema-postgres.sql
+++ data/schema-postgres.sql
@@ -1,33 +1,37 @@
 BEGIN;
 
 CREATE TABLE app_user (
-    app_user_id     INTEGER CONSTRAINT PK_APP_USER PRIMARY KEY AUTOINCREMENT,
+    app_user_id     SERIAL  NOT NULL,
     email           TEXT    NOT NULL UNIQUE,
     full_name       TEXT    NOT NULL,
     password        TEXT,
     reset_key       TEXT    NULL UNIQUE,
     api_key         TEXT    NULL UNIQUE,
     status          TEXT    NOT NULL DEFAULT 'active',
-    admin           BOOLEAN NOT NULL DEFAULT '0'
+    admin           BOOLEAN NOT NULL DEFAULT '0',
+    PRIMARY KEY (app_user_id)
 );
 
 CREATE TABLE wr_system (
-    wr_system_id    INTEGER CONSTRAINT PK_WR_SYSTEM PRIMARY KEY AUTOINCREMENT,
+    wr_system_id    SERIAL  NOT NULL,
     name            TEXT    NOT NULL,
     description     TEXT    NOT NULL,
     request_url     TEXT    NOT NULL DEFAULT '',
     colour_code     INTEGER NOT NULL DEFAULT '0',
-    is_default      BOOLEAN NOT NULL DEFAULT false
+    is_default      BOOLEAN NOT NULL DEFAULT false,
+    PRIMARY KEY (wr_system_id)
 );
 
 CREATE TABLE activity (
-    activity_id     INTEGER CONSTRAINT PK_ACTIVITY PRIMARY KEY AUTOINCREMENT,
+    activity_id     SERIAL   NOT NULL,
     app_user_id     INTEGER  NOT NULL,
-    date_time       DATETIME NOT NULL,
+    date_time       TIMESTAMP NOT NULL,
     duration        INTEGER  NOT NULL,
     wr_system_id    INTEGER  NOT NULL,
     wr_number       TEXT,
     description     TEXT,
+
+    PRIMARY KEY (activity_id),
 
     CONSTRAINT FK_ACTIVITY_USER
         FOREIGN KEY (app_user_id) REFERENCES app_user (app_user_id),
@@ -37,15 +41,16 @@ CREATE TABLE activity (
 );
 
 CREATE TABLE user_preference (
-    preference_id   INTEGER CONSTRAINT PK_PREFERENCE PRIMARY KEY AUTOINCREMENT,
+    preference_id   SERIAL NOT NULL,
     app_user_id     INTEGER NOT NULL,
     preference      TEXT NOT NULL,
-    value           TEXT NOT NULL
+    value           TEXT NOT NULL,
+    PRIMARY KEY(preference_id)
 );
 
 CREATE TABLE user_wr_system (
-    wr_system_id INTEGER NOT NULL,
-    app_user_id INTEGER NOT NULL,
+    "wr_system_id" INTEGER NOT NULL,
+    "app_user_id" INTEGER NOT NULL,
     PRIMARY KEY (wr_system_id, app_user_id),
 
     CONSTRAINT FK_USER_M2M
