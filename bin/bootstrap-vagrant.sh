c046c5a9fc669bbb112ba229f0b41c15c8be314c
Sam Crawley <samc@samc-pc.wgtn.cat-it.co.nz>
2014-06-30 09:43:54 +1200

    Added some extra things to vagrant bookstrap

--- bin/bootstrap-vagrant.sh
+++ bin/bootstrap-vagrant.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get -y install liblocal-lib-perl sqlite3 curl postgresql-9.1 libdbix-class-perl libdancer-perl libdbd-pg-perl libdbix-class-schema-loader-perl libhtml-fillinform-perl libdatetime-perl libtemplate-perl libjson-perl libdancer-plugin-dbic-perl libcss-minifier-xs-perl libwww-mechanize-perl libmime-lite-perl libdatetime-format-pg-perl curl
+DEBIAN_FRONTEND=noninteractive apt-get -y install liblocal-lib-perl sqlite3 curl postgresql-9.1 libdbix-class-perl libdancer-perl libdbd-pg-perl libdbix-class-schema-loader-perl libhtml-fillinform-perl libdatetime-perl libtemplate-perl libjson-perl libdancer-plugin-dbic-perl libcss-minifier-xs-perl libwww-mechanize-perl libmime-lite-perl libdatetime-format-pg-perl curl make
 
 curl -L http://cpanmin.us | perl - App::cpanminus
 
@@ -11,5 +11,8 @@ sudo -u postgres createuser -d --no-password --no-superuser --no-createrole vagr
 sudo -u vagrant createdb tksweb-vagrant
 sudo -u vagrant psql -f /vagrant/data/schema-postgres.sql tksweb-vagrant
 sudo -u vagrant psql -f /vagrant/data/vagrant_user.sql tksweb-vagrant
+
+mkdir -p /vagrant/var/dev-sessions
+
 
 
